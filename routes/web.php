77e7198eaf0db5c9fb8f7cc8c319b42fad393469
Nazwa <nazwaamanda251@gmail.com>
2026-05-31 18:32:41 +0800

    Nazwa: Memperbaiki routes dan halaman detail idol

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes - IdolVerse Kelompok 6
+|--------------------------------------------------------------------------
+*/
+
+// 1. Halaman Publik / Fans
+Route::get('/', function () { 
+    return view('welcome'); 
+});
+
+Route::get('/idols', function () { 
+    return view('welcome'); 
+});
+
+Route::get('/idols/jennie', function () { 
+    return view('idols.show'); 
+});
+
+// 2. Halaman Autentikasi
+Route::get('/login', function () { 
+    return view('login'); 
+});
+
+Route::get('/register', function () { 
+    return view('register'); 
+});
+
+// 3. Halaman Admin Panel
+Route::get('/admin/dashboard', function () { 
+    return view('dashboard'); 
+});
+
+Route::get('/admin/idols/create', function () { 
+    return view('form_idol'); 
+});
